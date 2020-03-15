4ab901c50de11164e74ee4bae5bd63cc32a959e3
santhosh <bsksr143@gmail.com>
2020-03-15 20:09:42 +0530

    change value

--- src/com/java8/StreamAPI.java
+++ src/com/java8/StreamAPI.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class StreamAPI {
 	public static void main(String[] args) {
 		List<Integer> l= new ArrayList<>();
-		for(int i=1; i<=100; i++) {
+		for(int i=1; i<=10; i++) {
 			l.add(i);
 		}
 		l.forEach(i-> System.out.print(i+" "));
